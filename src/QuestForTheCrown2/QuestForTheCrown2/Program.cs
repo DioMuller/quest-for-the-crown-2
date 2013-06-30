f81b67798f5ae961433d3dd38d0a320f24b9ea6d
Joao Vitor P. Moraes <jvlppm@gmail.com>
2013-06-30 19:43:56 -0300

    INT: Incluído dlls do MonoGame e suas dependencias.

--- src/QuestForTheCrown2/QuestForTheCrown2/Program.cs
+++ src/QuestForTheCrown2/QuestForTheCrown2/Program.cs
@@ -12,12 +12,22 @@ namespace QuestForTheCrown2
     /// </summary>
     public static class Program
     {
+        static void SetupLibraries()
+        {
+            #region Include Libraries to path
+            string dllDir = @".\lib\" + (Environment.Is64BitProcess ? "x64" : "x86");
+            Environment.SetEnvironmentVariable("PATH", dllDir + ";" + Environment.GetEnvironmentVariable("PATH"));
+            #endregion
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            SetupLibraries();
+
             using (var game = new GameMain())
                 game.Run();
         }
