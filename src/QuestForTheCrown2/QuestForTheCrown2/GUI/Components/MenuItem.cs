eb32c47b1cb2465a83757e7d7a1b659e913d0ff9
Diogo Muller <diogo.muller@live.com>
2013-07-06 20:05:31 -0300

    INT: Basic title screen (not being shown) and base GUI component.

---
+++ src/QuestForTheCrown2/QuestForTheCrown2/GUI/Components/MenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace QuestForTheCrown2.GUI.Components
+{
+    public class MenuItem : Component
+    {
+        public string Value { get; set; }
+
+        public MenuItem(string value) : base()
+        {
+        }
+
+        /// <summary>
+        /// Draws the Component
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
---
+++ src/QuestForTheCrown2/QuestForTheCrown2/Screens/TitleScreen.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using QuestForTheCrown2.Base;
+
+namespace QuestForTheCrown2.Screens
+{
+    class TitleScreen
+    {
+        private Texture2D _logo;
+        private Rectangle _logoPosition;
+
+        public TitleScreen(Rectangle window)
+        {
+            _logo = GameContent.LoadContent<Texture2D>("images/logo.png");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_logo, _logoPosition, Color.White);
+        }
+
+        private void UpdatePositions(Rectangle window)
+        {
+            _logoPosition = new Rectangle(Convert.ToInt32((window.Width - window.X) / 2f - (_logo.Width / 2)), Convert.ToInt32(0.3f * (window.Height - window.Y)), _logo.Width, _logo.Height);
+        }
+    }
+}
