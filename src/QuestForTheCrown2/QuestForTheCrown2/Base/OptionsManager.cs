3acb4f02db990ea5c8e8b32fd7a6cdda32210dec
Diogo Muller <diogo.muller@live.com>
2013-07-10 22:56:16 -0300

    WIP: Started working on #16, but had some problems with the storage class. Added functionality initial idea.

---
+++ src/QuestForTheCrown2/QuestForTheCrown2/Base/OptionsManager.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace QuestForTheCrown2.Base
+{
+    public class Options
+    {
+        #region Graphical Options
+        /// <summary>
+        /// Resolution ( Width )
+        /// </summary>   
+        public int ResolutionWidth { get; set; }
+        
+        /// <summary>
+        /// Resolution ( Height )
+        /// </summary>
+        public int ResolutionHeight { get; set; }
+
+        /// <summary>
+        /// Is the game fullscreen?
+        /// </summary>
+        public bool Fullscreen { get; set; }
+        #endregion Graphical Options
+
+        #region Controller Options
+        /// <summary>
+        /// Invert bow aim?
+        /// </summary>
+        public bool InvertAim { get; set; }
+        #endregion Controller Options
+
+        #region Constructor
+        /// <summary>
+        /// Creates an Options instance with the default settings.
+        /// </summary>
+        public Options()
+        {
+            ResolutionWidth = 1280;
+            ResolutionHeight = 720;
+            Fullscreen = true;
+            InvertAim = false;
+        }
+        #endregion Constructor
+    }
+
+    public static class OptionsManager
+    {
+        /// <summary>
+        /// Game File.
+        /// </summary>
+        public const string OptionsFile = "GameOptions.xml";
+
+        /// <summary>
+        /// Current Options
+        /// </summary>
+        public static Options CurrentOptions { get; private set; }
+
+        /// <summary>
+        /// Loads the game options.
+        /// </summary>
+        public static void LoadOptions()
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if( !store.FileExists(OptionsFile) )
+                {
+                    store.CreateFile(OptionsFile);
+                    CurrentOptions = new Options();
+                }
+                else
+                {
+                    string content;
+
+                    using (StreamReader sr = new StreamReader(store.OpenFile(OptionsFile, FileMode.Open)))
+                    {
+                        content = sr.ReadToEnd();
+                    }
+                 
+                    XDocument doc = XDocument.Parse(content);
+                    XElement root = doc.Element("options");
+
+                    CurrentOptions = new Options
+                    {
+                        ResolutionWidth = int.Parse(root.Element("ResolutionWidth").Value),
+                        ResolutionHeight = int.Parse(root.Element("ResolutionHeight").Value),
+                        Fullscreen = bool.Parse(root.Element("Fullscreen").Value),
+                        InvertAim = bool.Parse(root.Element("InvertAim").Value)
+                    };
+                }
+            }
+        }
+
+        public static void SaveOptions()
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (CurrentOptions == null)
+                {
+                    CurrentOptions = new Options();
+                }
+
+                List<string> content = new List<string>();
+
+                content.Add("<options>");
+
+                content.Add("<ResolutionWidth>" + CurrentOptions.ResolutionWidth.ToString() + "</ResolutionWidth>");
+                content.Add("<ResolutionHeight>" + CurrentOptions.ResolutionHeight.ToString() + "</ResolutionHeight>");
+                content.Add("<Fullscreen>" + CurrentOptions.Fullscreen.ToString() + "</Fullscreen>");
+                content.Add("<InvertAim>" + CurrentOptions.InvertAim.ToString() + "</InvertAim>");
+
+                content.Add("</options>");
+
+                if (store.FileExists(OptionsFile)) store.DeleteFile(OptionsFile);
+
+                using (StreamWriter sw = new StreamWriter(store.OpenFile(OptionsFile, FileMode.Open)))
+                {
+                    foreach( string st in content )
+                    {
+                        sw.WriteLine(st);
+                    }
+                }
+            }
+        }
+    }
+}
