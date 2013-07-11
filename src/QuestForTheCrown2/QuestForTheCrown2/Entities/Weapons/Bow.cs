a641473181e45b0a2e0f720755993e4aa34d314b
Diogo Muller <diogo.muller@live.com>
2013-07-11 20:51:28 -0300

    FET: #18 - Create Options screen. Working Options screen.

--- src/QuestForTheCrown2/QuestForTheCrown2/Entities/Weapons/Bow.cs
+++ src/QuestForTheCrown2/QuestForTheCrown2/Entities/Weapons/Bow.cs
@@ -40,7 +40,7 @@ namespace QuestForTheCrown2.Entities.Weapons
             Position = Parent.CenterPosition;
 
             if (direction == Vector2.Zero)
-                direction = Parent.CurrentDirection * -1;
+                direction = Parent.CurrentDirection * (OptionsManager.CurrentOptions.InvertAim?-1:1);
 
             Angle = (float)(Math.Atan2(direction.X, -direction.Y) + _spriteAngle);
 
